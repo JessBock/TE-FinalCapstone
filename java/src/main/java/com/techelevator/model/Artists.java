267b51bd4e570f7d7d309950d0204d5856727835
Jessica Bock[D[D[D[D[D[D[D[D[D[D[j[C[C[C[C[C[C[C[b[C[C[C <j.a.johnston35@gmail.com>
2022-04-11 17:36:08 -0400

    Updates to Discogs Service - Added Classes

---
+++ java/src/main/java/com/techelevator/model/Artists.java
@@ -0,0 +1,31 @@
+package com.techelevator.model;
+
+public class Artists {
+    private long id;
+    private String name;
+
+    public Artists() {
+        super();
+    }
+
+    public Artists(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
