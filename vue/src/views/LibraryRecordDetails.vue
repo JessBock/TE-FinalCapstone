18f5aa517434e4d9cfd93883f7d969c5e1011b4b
Jessica Bock[D[D[D[D[D[D[D[D[D[D[j[C[C[C[C[C[C[C[b[C[C[C <j.a.johnston35@gmail.com>
2022-04-18 18:09:46 -0400

    Record Details Styling

--- vue/src/views/LibraryRecordDetails.vue
+++ vue/src/views/LibraryRecordDetails.vue
@@ -1,36 +1,44 @@
 <template>
   <div class='recordDetails'>
-      <div class="recordImage">
+    <div class="recordImage">
       <img v-bind:src="details.coverImg" />
       <h1>{{details.artists[0].name}}</h1>
       <h2>{{details.title}}</h2>
-      </div>
-      <div class="text">
-      
-      <div v-for="track in details.tracklist" v-bind:key="track.position">
-          {{track.title}} : {{track.duration}} - {{track.position}}
-      </div>
-      <select v-model.number='collectionId' >
-          <option v-for="collection in $store.state.collection"  v-bind:value="collection.collectionId" v-bind:key="collection.collectionId">{{collection.collectionName}}</option>
-      </select>
-      <button type=submit v-on:click.prevent="saveToCollection()">Add to Collection</button>
-
-
-      <form v-on:submit.prevent="setCommentsAndCondition">
-            <label for="condition">Record Condition:</label>
-            <select id="condition" v-model="details.condition">
-                <option value="Mint">Mint</option>
-                <option value="Near Mint">Near Mint</option>
-                <option value="Good">Good</option>
-                <option value="Fair">Fair</option>
-                <option value="Poor">Poor</option>
+    </div>
+    <div class="text">
+        <div class="collection-form">
+            <label for="collection">Collection:</label>
+            <select id="collection"  v-model.number='collectionId' >
+                <option v-for="collection in $store.state.collection"  v-bind:value="collection.collectionId" v-bind:key="collection.collectionId">{{collection.collectionName}}</option>
             </select>
-            <label for="comments">Comments:</label>
-            <textarea type="text" id="comments" v-model="details.comments"></textarea>
-            <button>Save Changes</button>
-      </form>
+            <button type=submit v-on:click.prevent="saveToCollection()">Add to Collection</button>
+        </div>
 
-      </div>
+        <div class="tracks">
+            <h2>Tracks:</h2>
+            <div v-for="track in details.tracklist" v-bind:key="track.position">
+            {{track.title}} : {{track.duration}} - {{track.position}}
+            </div>
+        </div> 
+
+            <form class="comment-form" v-on:submit.prevent="setCommentsAndCondition">
+                <div>
+                <label for="condition">Record Condition:</label>
+                <select id="condition" v-model="details.condition">
+                    <option value="Mint">Mint</option>
+                    <option value="Near Mint">Near Mint</option>
+                    <option value="Good">Good</option>
+                    <option value="Fair">Fair</option>
+                    <option value="Poor">Poor</option>
+                </select>
+                </div>
+                <div>
+                <label for="comments">Comments:</label>
+                <textarea type="text" id="comments" v-model="details.comments"></textarea>
+                </div>
+                <button>Save Changes</button>
+            </form>
+    </div>
   </div>
 </template>
 
@@ -82,19 +90,50 @@ export default {
 
 <style>
 .text {
+    display: flex;
+    flex-direction: column;
     background-color: black;
     color: white;
     margin: 1vw;
+    flex-grow: 2;
     padding: 20px;
-    width: 30vw;
     justify-content: center;
     align-items: center;
     text-align: center;
     border-radius: 20px;
 }
+
+.text div {
+    flex-grow: 1;
+    display: flex;
+    align-items: center;
+    justify-items: center;
+}
+
+.tracks {
+    flex-direction: column;
+}
+
+.comment-form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 5px;
+}
+
+select {
+    margin: 5px;
+}
+
+textarea {
+    margin: 5px;
+}
+
 .recordDetails{
     display: flex;
     margin: auto;
+    flex-grow: 1;
     
 }
 
